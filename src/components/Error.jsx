683c50a4dd16508c8d2be35c9cf8099a92281387
Divy Parekh <divyparekh1810@gmail.com>
2024-07-21 01:11:26 +0530

    dark mode implementation bug fixes

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,12 +1,21 @@
 "use client";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Error = ({ error }) => {
+  const darkMode = useSelector((state) => state.theme.darkMode);
+
   return (
-    <div className="w-full h-full flex items-center justify-center ">
-      <div className="bg-gray-700 shadow-md rounded-lg p-6 text-center">
+    <div className="w-full h-full flex items-center justify-center">
+      <div
+        className={`shadow-md rounded-lg p-6 text-center ${
+          darkMode ? "bg-gray-900 text-gray-100" : "bg-gray-200 text-gray-800"
+        }`}
+      >
         <svg
-          className="h-12 w-12 text-red-600 mb-4 mx-auto"
+          className={`h-12 w-12 mb-4 mx-auto ${
+            darkMode ? "text-red-500" : "text-red-600"
+          }`}
           fill="none"
           stroke="currentColor"
           strokeWidth="2"
@@ -19,7 +28,7 @@ const Error = ({ error }) => {
             d="M12 9v2m0 4h.01M12 17a1 1 0 100-2 1 1 0 000 2zm0 6a9 9 0 100-18 9 9 0 000 18z"
           />
         </svg>
-        <p className="text-lg text-gray-800 mb-4">{error}</p>
+        <p className="text-lg mb-4">{error}</p>
       </div>
     </div>
   );
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,21 @@
 "use client";
+import React from "react";
+import { useSelector } from "react-redux";
+
 const Loading = () => {
+  const darkMode = useSelector((state) => state.theme.darkMode);
+
   return (
-    <div className="bg-gray-700 flex items-center justify-center h-screen">
+    <div
+      className={`flex items-center justify-center h-screen ${
+        darkMode ? "bg-gray-900" : "bg-gray-200"
+      }`}
+    >
       <div className="flex flex-col items-center">
         <svg
-          className="animate-spin h-12 w-12 text-blue-600 mb-4"
+          className={`animate-spin h-12 w-12 mb-4 ${
+            darkMode ? "text-blue-400" : "text-blue-600"
+          }`}
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -23,7 +34,11 @@ const Loading = () => {
             d="M4 12a8 8 0 018-8v8H4z"
           ></path>
         </svg>
-        <p className="text-lg text-gray-600">Loading...</p>
+        <p
+          className={`text-lg ${darkMode ? "text-gray-100" : "text-gray-800"}`}
+        >
+          Loading...
+        </p>
       </div>
     </div>
   );
